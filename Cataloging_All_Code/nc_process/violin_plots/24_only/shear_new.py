Fixing tick marks and units

Cataloging_All_Code/nc_process/violin_plots/24_only/shear_new.py
@@ -47,7 +47,7 @@ def label_every_20(x, pos):
     :param x: Tick marks
     :return: Tick labels
     """
-    return f"{int(x)}" if x % 20 == 0 else ''
+    return f"{int(x)}" if x % 40 == 0 else ''
 
 
 def group_func(data: list, group_labels: list):
@@ -258,7 +258,7 @@ for z, (ax, data, label, labels) in enumerate(zip(axes.flatten(), data_list, lab
         ax.set_xlim((-85, 85))
         ax.set_xticks(range(-80, 81, 20))
     ax.xaxis.set_major_formatter(FuncFormatter(label_every_20))
-    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(ax.get_xticklabels())
     # Ensure there's enough space at the top
     ymin = min([np.min(d) for d in data])
     ymax = max([np.max(d) for d in data])
@@ -345,7 +345,7 @@ for z, (ax, data, label, labels) in enumerate(zip(axes.flatten(), data_list, lab
             linestyle='None',
             zorder=3
         )
-    ax.set_xticklabels(labels, rotation=45, ha='right', fontsize=18)
+    ax.set_xticklabels(labels, fontsize=18)
     ax.set_yticklabels(labels, fontsize=18)
     if '6' in label:
         ax.set_xlim((-65, 45))
@@ -366,7 +366,7 @@ for z, (ax, data, label, labels) in enumerate(zip(axes.flatten(), data_list, lab
     ax.set_xticks(tick_marks)
     ax.xaxis.set_major_formatter(FuncFormatter(label_every_20))
     ax.set_yticks(tick_marks)
-    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(ax.get_xticklabels())
     ax.yaxis.set_major_formatter(FuncFormatter(label_every_20))
     plt.setp(ax.get_yticklabels())
     ax.set_title(f'{label}', fontsize=20)
@@ -473,7 +473,7 @@ for z, (ax, data, label, labels) in enumerate(zip(axes.flatten(), data_list, lab
         ax.set_xlim((-105, 85))
         ax.set_xticks(range(-100, 81, 20))
     ax.xaxis.set_major_formatter(FuncFormatter(label_every_20))
-    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(ax.get_xticklabels())
     # Ensure there's enough space at the top
     ymin = min([np.min(d) for d in data])
     ymax = max([np.max(d) for d in data])
@@ -560,7 +560,7 @@ for z, (ax, data, label, labels) in enumerate(zip(axes.flatten(), data_list, lab
             linestyle='None',
             zorder=3
         )
-    ax.set_xticklabels(labels, rotation=45, ha='right', fontsize=18)
+    ax.set_xticklabels(labels, fontsize=18)
     ax.set_yticklabels(labels, fontsize=18)
     if '6' in label:
         ax.set_xlim((-65, 45))
@@ -581,7 +581,7 @@ for z, (ax, data, label, labels) in enumerate(zip(axes.flatten(), data_list, lab
     ax.set_xticks(tick_marks)
     ax.xaxis.set_major_formatter(FuncFormatter(label_every_20))
     ax.set_yticks(tick_marks)
-    plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(ax.get_xticklabels())
     ax.yaxis.set_major_formatter(FuncFormatter(label_every_20))
     plt.setp(ax.get_yticklabels())
     ax.set_title(f'{label}', fontsize=20)

Cataloging_All_Code/nc_process/violin_plots/humidity_sst_all/shear_new.py
@@ -189,8 +189,10 @@ def plot_violin_2panel(data1, labels1, data2, labels2, suptitle, xlabel, ylabel,
 
         # --- Formatting ---
         ax.set_xlim((17, 33))
-        ax.set_xticks(range(18, 33, 1))
-        ax.set_xticklabels(np.arange(18, 33, 1), rotation=45, ha='right')
+        tick_marks = range(18, 33, 1)
+        labels = [str(t) if i % 2 == 0 else '' for i, t in enumerate(tick_marks)]
+        ax.set_xticks(tick_marks)
+        ax.set_xticklabels(labels, rotation=0)
         ax.set_ylim((0, 60))
         ax.set_title(panel_title, fontsize=28)
         ax.tick_params(axis='both', labelsize=18)
@@ -269,8 +271,9 @@ def plot_contourf_2panel(data1: list, labels1: list, data2: list, labels2: list,
     axes[0].set_ylim((17, 33))
     axes[0].set_xticks(tick_marks)
     axes[0].set_yticks(tick_marks)
-    axes[0].set_xticklabels(tick_marks, rotation=45, ha='right')
-    axes[0].set_yticklabels(tick_marks)
+    labels = [str(t) if i % 2 == 0 else '' for i, t in enumerate(tick_marks)]
+    axes[0].set_xticklabels(labels, rotation=0)
+    axes[0].set_yticklabels(labels)
     axes[0].tick_params(axis='both', labelsize=18)
     axes[0].set_title('Atlantic', fontsize=28)
     axes[0].grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
@@ -306,8 +309,9 @@ def plot_contourf_2panel(data1: list, labels1: list, data2: list, labels2: list,
     axes[1].set_ylim((17, 33))
     axes[1].set_xticks(tick_marks)
     axes[1].set_yticks(tick_marks)
-    axes[1].set_xticklabels(tick_marks, rotation=45, ha='right')
-    axes[1].set_yticklabels(tick_marks)
+    labels = [str(t) if i % 2 == 0 else '' for i, t in enumerate(tick_marks)]
+    axes[1].set_xticklabels(labels, rotation=0)
+    axes[1].set_yticklabels(labels)
     axes[1].tick_params(axis='both', labelsize=18)
     axes[1].set_title('Eastern Pacific', fontsize=28)
     axes[1].grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)

Cataloging_All_Code/nc_process/violin_plots/shear_process_all/shear_new.py
@@ -204,7 +204,7 @@ def plot_violin_2panel_diurnal(data1, labels1, data2, labels2, suptitle, xlabel,
         # --- Formatting ---
         ax.set_xlim((-2, 23))
         ax.set_xticks(range(0, 24, 3))
-        ax.set_xticklabels(np.arange(0, 24, 3), rotation=45, ha='right')
+        ax.set_xticklabels(np.arange(0, 24, 3))
         ax.set_ylim((0, 60))
         ax.set_title(panel_title, fontsize=28)
         ax.grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
@@ -302,7 +302,7 @@ def plot_contourf_2panel_diurnal(data1: list, labels1: list, data2: list, labels
     axes[0].set_ylim((-1, 22))
     axes[0].set_xticks(tick_marks)
     axes[0].set_yticks(tick_marks)
-    axes[0].set_xticklabels(tick_marks, rotation=45, ha='right')
+    axes[0].set_xticklabels(tick_marks)
     axes[0].set_yticklabels(tick_marks)
     axes[0].set_title('Atlantic', fontsize=28)
     axes[0].grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
@@ -339,7 +339,7 @@ def plot_contourf_2panel_diurnal(data1: list, labels1: list, data2: list, labels
     axes[1].set_ylim((-1, 23))
     axes[1].set_xticks(tick_marks)
     axes[1].set_yticks(tick_marks)
-    axes[1].set_xticklabels(tick_marks, rotation=45, ha='right')
+    axes[1].set_xticklabels(tick_marks)
     axes[1].set_yticklabels(tick_marks)
     axes[1].set_title('Eastern Pacific', fontsize=28)
     axes[1].grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
@@ -425,8 +425,11 @@ def plot_violin_2panel_intensity(data1, labels1, data2, labels2, suptitle, xlabe
 
         # --- Formatting ---
         ax.set_xlim((15, 165))
-        ax.set_xticks(range(20, 161, 10))
-        ax.set_xticklabels(np.arange(20, 161, 10), rotation=45, ha='right')
+        ticks = np.arange(20, 161, 10)
+        labels = [str(t) if i % 2 == 0 else '' for i, t in enumerate(ticks)]
+
+        ax.set_xticks(ticks)
+        ax.set_xticklabels(labels, rotation=0)
         ax.set_ylim((0, 60))
         ax.set_title(panel_title, fontsize=28)
         ax.grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
@@ -525,10 +528,11 @@ def plot_contourf_2panel_intensity(data1: list, labels1: list, data2: list, labe
         )
     axes[0].set_xlim((15, 165))
     axes[0].set_ylim((15, 165))
+    labels = [str(t) if i % 2 == 0 else '' for i, t in enumerate(tick_marks)]
     axes[0].set_xticks(tick_marks)
+    axes[0].set_xticklabels(labels, rotation=0)
     axes[0].set_yticks(tick_marks)
-    axes[0].set_xticklabels(tick_marks, rotation=45, ha='right')
-    axes[0].set_yticklabels(tick_marks)
+    axes[0].set_yticklabels(labels)
     axes[0].set_title('Atlantic', fontsize=28)
     axes[0].grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
     axes[0].tick_params(axis='both', labelsize=18)
@@ -562,10 +566,11 @@ def plot_contourf_2panel_intensity(data1: list, labels1: list, data2: list, labe
         )
     axes[1].set_xlim((15, 165))
     axes[1].set_ylim((15, 165))
+    labels = [str(t) if i % 2 == 0 else '' for i, t in enumerate(tick_marks)]
     axes[1].set_xticks(tick_marks)
+    axes[1].set_xticklabels(labels, rotation=0)
     axes[1].set_yticks(tick_marks)
-    axes[1].set_xticklabels(tick_marks, rotation=45, ha='right')
-    axes[1].set_yticklabels(tick_marks)
+    axes[1].set_yticklabels(labels)
     axes[1].set_title('Eastern Pacific', fontsize=28)
     axes[1].grid(True, color='black', linestyle='--', linewidth=1.0, alpha=1)
     axes[1].tick_params(axis='both', labelsize=18)